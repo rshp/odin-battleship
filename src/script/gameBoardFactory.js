8c22df3484114c6f25c1d088ef395dfc3e3f9ef3
shpr <roman.shpr@gmail.com>
2022-06-13 20:55:10 +0300

    Change var banes

--- src/script/gameBoardFactory.js
+++ src/script/gameBoardFactory.js
@@ -1,5 +1,5 @@
 export default (boardSize) => {
-	function boardInit(boardSize) {
+	function boardInit(size) {
 		const initialContent = {
 			occupancy: {
 				occupied: 0,
@@ -10,13 +10,11 @@ export default (boardSize) => {
 			validForPlacement: 1,
 		};
 
-		const cells = new Array(boardSize)
-			.fill(0)
-			.map((x) => Array(boardSize).fill(0));
+		const cells = new Array(size).fill(0).map(() => Array(size).fill(0));
 
-		for (let i = 0; i < boardSize; i += 1) {
-			for (let j = 0; j < boardSize; j += 1) {
-				cells[i][j] = JSON.parse(JSON.stringify(initialContent));
+		for (let i = 0; i < size; i += 1) {
+			for (let j = 0; j < size; j += 1) {
+				cells[i][j] = JSON.parse(JSON.stringify(initialContent)); // deep copy object
 			}
 		}
 		return cells;
@@ -27,22 +25,18 @@ export default (boardSize) => {
 
 	function placeShip(ship, coords, orientation) {
 		ships.push(ship);
-		if (orientation === 'horizontal') {
-			for (let index = 0; index < ship.length; index += 1) {
-				const currentCell = cell[coords[0] + index][coords[1]];
-				currentCell.occupancy.occupied = 1;
-				currentCell.occupancy.ship = ship;
-				currentCell.occupancy.shipSegment = index;
-			}
-		}
-		if (orientation === 'vertical') {
-			for (let index = 0; index < ship.length; index += 1) {
-				const currentCell = cell[coords[0]][coords[1] + index];
-				currentCell.occupancy.occupied = 1;
-				currentCell.occupancy.ship = ship;
-				currentCell.occupancy.shipSegment = index;
-			}
+
+		for (let index = 0; index < ship.length; index += 1) {
+			let currentCell;
+			if (orientation === 'horizontal')
+				currentCell = cell[coords[0] + index][coords[1]];
+			if (orientation === 'vertical')
+				currentCell = cell[coords[0]][coords[1] + index];
+			currentCell.occupancy.occupied = 1;
+			currentCell.occupancy.ship = ship;
+			currentCell.occupancy.shipSegment = index;
 		}
 	}
+
 	return { cell, ships, placeShip };
 };
