8e4a7d72c905e3a67fe6061c0b3b3874d57383f7
shpr <roman.shpr@gmail.com>
2022-06-13 13:39:46 +0300

    Implement board initialization

--- src/script/gameBoardFactory.js
+++ src/script/gameBoardFactory.js
@@ -1,4 +1,23 @@
 export default (boardSize) => {
-	const board = null;
-	return { board };
+	function boardInit(boardSize) {
+		const initialContent = {
+			occupancy: {
+				occupied: 0,
+				ship: null,
+				shipSegment: null,
+			},
+			wasHit: 0,
+			validForPlacement: 1,
+		};
+
+		const board = new Array(boardSize).fill(
+			new Array(boardSize).fill(initialContent)
+		);
+		return board;
+	}
+
+	const cell = boardInit(boardSize);
+	const ships = [];
+
+	return { cell, ships };
 };
